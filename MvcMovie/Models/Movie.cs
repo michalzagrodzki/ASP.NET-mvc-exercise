aedd2868f8fe3eb1e6c994ba09d884c8e845100e
Michal Zagrodzki <michalzagrodzki@gmail.com>
2015-08-16 15:53:02 +0200

    Add validation to Movie model.

--- MvcMovie/Models/Movie.cs
+++ MvcMovie/Models/Movie.cs
@@ -7,7 +7,9 @@ namespace MvcMovie.Models
     public class Movie
     {
         public int ID { get; set; }
-
+        
+        [StringLength(60, MinimumLength=3)]
+        [Required]
         public string Title { get; set; }
 
         [Display( Name = "Release Date" )]
@@ -15,10 +17,18 @@ namespace MvcMovie.Models
         [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
         public DateTime ReleaseDate { get; set; }
 
+        [RegularExpression(@"^[A-Z]+[a-zA-Z''-'\s]*$")]
+        [Required]
+        [StringLength(30)]
         public string Genre { get; set; }
 
+        [Range(1, 100)]
+        [DataType(DataType.Currency)]
         public decimal Price { get; set; }
 
+        [RegularExpression(@"^[A-Z]+[a-zA-Z''-'\s]*$")]
+        [Required]
+        [StringLength(5)]
         public string Rating { get; set; }
     }
 
